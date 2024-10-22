3ab11fbde562b058db74014d3b0cb4224e02e963
Varvara Folimonova <varvarafolimonova@gmail.com>
2024-10-22 12:08:45 -0400

    fixed error

--- decoder.py
+++ decoder.py
@@ -5,5 +5,8 @@ def decoder(encoded_pass):
         de_digit = int(digit)-3
         if de_digit <= 0:
             de_digit += 10
-        decoded += str(de_digit)
+        decoded += str(de_digit)[-1]
     print(f"The encoded password is {encoded_pass}, and the original password is {decoded}.")
+
+
+    
